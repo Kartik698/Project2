d5960b24ee7c37d6eb3677286ce450217cfcfe5f
Kartik698 <kartikkn93@gmail.com>
2025-06-08 11:13:10 +0530

    Add files via upload
    JavaScript file of  portfolio

---
+++ Protoflio.js
@@ -0,0 +1,15 @@
+let linkedin = document.querySelector("#linkedin");
+let github = document.querySelector("#github");
+let hirebtn = document.querySelector("#hirebtn");
+
+linkedin.addEventListener("click", ()=>{
+    window.location.href  = "https://www.linkedin.com/in/kartik-sharma-71b116306/"
+})
+
+github.addEventListener("click" , ()=>{
+     window.location.href  = "https://github.com/Kartik698"
+})
+
+hirebtn.addEventListener("click", ()=>{
+    window.location.href  = "https://www.linkedin.com/in/kartik-sharma-71b116306/"
+})
